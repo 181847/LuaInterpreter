0fe5e1263649a4bd4c5038acbc567fd210ff0d47
181847 <753090084@qq.com>
2017-11-29 21:13:25 +0800

    将原本LuaInterpreterUnitTest中的代码转换过来，并且添加了一个对交互模式的测试。元

---
+++ TryUnitTestModule/TryUnitTestModule.cpp
@@ -0,0 +1,218 @@
+// TryUnitTestModule.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "../LuaInterpreterUnitTest/UnitTestModules.h"
+#include "../../Library/Lua/LuaModuls/LuaMeshData/LuaMeshDataStruct.h"
+#include "../../Library/Lua/LuaInterpreter/LuaInterpreter.h"
+#include "../../Library/MyTools/MyAssert.h"
+#include "../../Library/MyTools/Formater.h"
+
+#pragma comment(lib, "LuaInterpreter.lib")
+
+DECLARE_TEST_UNITS;
+
+static auto GLuaInterpreter = std::make_unique<Lua::LuaInterpreter>();
+
+#define TEST_TARGET GLuaInterpreter
+
+// this function is declared in the UnitTestModuls.h,
+// we should implements it here.
+void TestUnit::GetReady()
+{
+	TEST_TARGET->DoFile("Init.lua");
+}
+
+// this function is declared int the UnitTestModuls.h
+// we should implements it here
+void TestUnit::AfterTest()
+{
+	TEST_TARGET->Run();
+}
+
+void TestUnit::AddTestUnit()
+{
+	TEST_UNIT_START("doFile is called successfully, \
+		if you can see this information")
+		return true;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("test get global name")
+		TEST_TARGET->GetGlobal("test_GetGlobal");
+		int isNum;
+		int itg = lua_tointegerx(TEST_TARGET->m_L, -1, &isNum);
+		return (isNum && itg == 6);
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("test stack size")
+		int before = TEST_TARGET->GetStackSize();
+		TEST_TARGET->GetGlobal("test_GetGlobal");
+		before -= TEST_TARGET->GetStackSize();
+		return before == -1;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("test pop")
+		TEST_TARGET->GetGlobal("test_GetGlobal");
+		int before = TEST_TARGET->GetStackSize();
+		TEST_TARGET->Pop();
+		before -= TEST_TARGET->GetStackSize();
+		return before == 1;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("test to number and pop")
+		TEST_TARGET->GetGlobal("test_ToNumberAndPop_1");
+		float flt;
+		TEST_TARGET->ToNumberAndPop<float>(&flt);
+		return flt == 1;
+	TEST_UNIT_END; 
+	
+	TEST_UNIT_START("test to number and pop inner type")
+		TEST_TARGET->GetGlobal("test_ToNumberAndPop_2");
+		float flt = TEST_TARGET->ToNumberAndPop<float>();
+		return flt == 2;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("test to integer and pop")
+		TEST_TARGET->GetGlobal("test_ToIntegerAndPop_1");
+		int itg = TEST_TARGET->ToIntegerAndPop<int>();
+		return itg == 3;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("test to integer and pop inner type")
+		TEST_TARGET->GetGlobal("test_ToIntegerAndPop_2");
+		int itg = TEST_TARGET->ToIntegerAndPop<int>();
+		return itg == 4;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("test to integer and pop inner type")
+		TEST_TARGET->GetGlobal("test_ToStringandClear");
+		Formater<128> buffer;
+		TEST_TARGET->ToStringAndClear<buffer.Size>(buffer.bufferPointer());
+		return 0 == strcmp(buffer.bufferPointer(), "test_string_5");
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("get field on the table(on the top of the stack)")
+		TEST_TARGET->GetGlobal("test_table");
+		TEST_TARGET->GetFieldOnTop("a");
+		int itg = TEST_TARGET->ToIntegerAndPop<int>();
+		return itg == 1;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("get index on the table(on the top of the stack)")
+		TEST_TARGET->GetGlobal("test_table");
+		TEST_TARGET->GetIndexOnTop(1);
+		int itg = TEST_TARGET->ToIntegerAndPop<int>();
+		return itg == 4;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("can isNil work")
+		TEST_TARGET->GetGlobal("test_GetGlobal");
+		bool shouldBeFalse = TEST_TARGET->IsNil();
+		lua_pushnil(TEST_TARGET->m_L);
+		bool shouldBeTrue = TEST_TARGET->IsNil();
+		return Not(shouldBeFalse) && shouldBeTrue;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("traverse table")
+		TEST_TARGET->GetGlobal("test_table");
+		int error = 0;
+		TEST_TARGET->Foreach(
+			[&error](LUA_INTERPRETER_FOREACH_LAMBDA_ARGS){
+				// @ pLuaInter
+				int itg = luaInterForeach->ToIntegerAndPop<int>();
+				if (keyIsNumber)
+				{
+					switch (keyItg)
+					{
+					case 1:
+						error += 4 != itg;
+						break;
+
+					case 2:
+						error += 5 != itg;
+						break;
+
+					case 3:
+						error += 6 != itg;
+						break;
+					default:
+						++error;
+						break;
+					}
+					DEBUG_MESSAGE("key index: %d\n", keyItg);
+				}
+				else
+				{
+					if (0 == strcmp(keyStr, "a"))
+					{
+						error += 1 != itg;
+					}
+					else if (0 == strcmp(keyStr, "b"))
+					{
+						error += 2 != itg;
+					}
+					else if (0 == strcmp(keyStr, "c"))
+					{
+						error += 3 != itg;
+					}
+					else
+					{
+						++error;
+					}
+					DEBUG_MESSAGE("key field: %s\n", keyStr);
+				}
+	
+			}); // end foreach
+		return error == 0;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("test get userdata")
+		TEST_TARGET->GetGlobal("test_userdata_meshdata");
+		LuaPointerContainer<Lua::MeshData> * pmd = 
+			TEST_TARGET->ToUserDataAndClear<LuaPointerContainer<Lua::MeshData>>(
+				Lua::LuaMeshDataMetatableName);
+		int error = 0;
+		// remind that all the vector here contain a none placeholder
+		// which stand for the meanless component.
+		// so the avalible size should substract one
+		error += 1 != ( pmd->pointer->Positions	.size() - 1 );
+		error += 2 != ( pmd->pointer->Texcoords	.size() - 1 );
+		error += 3 != ( pmd->pointer->Normals	.size() - 1 );
+		error += 4 != ( pmd->pointer->TangentUs	.size() - 1 );
+		error += 5 != ( pmd->pointer->Vertices	.size() - 1 );
+		error += 6 != ( pmd->pointer->Indices32	.size() - 1 );
+		return error == 0;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("test get userdata inner type")
+		TEST_TARGET->GetGlobal("test_userdata_meshdata");
+		LuaPointerContainer<Lua::MeshData> * pmd = nullptr;
+		TEST_TARGET->ToUserDataAndClear<LuaPointerContainer<Lua::MeshData>>(
+			Lua::LuaMeshDataMetatableName, &pmd);
+		int error = 0;
+		// remind that all the vector here contain a none placeholder
+		// which stand for the meanless component.
+		// so the avalible size should substract one
+		error += 1 != ( pmd->pointer->Positions	.size() - 1 );
+		error += 2 != ( pmd->pointer->Texcoords	.size() - 1 );
+		error += 3 != ( pmd->pointer->Normals	.size() - 1 );
+		error += 4 != ( pmd->pointer->TangentUs	.size() - 1 );
+		error += 5 != ( pmd->pointer->Vertices	.size() - 1 );
+		error += 6 != ( pmd->pointer->Indices32	.size() - 1 );
+		return error == 0;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("test run the lua interpreter")
+		printf("Here have enter the lua interactive mode, please enter the commands to test the interpreter.\n");
+		printf("This unit test will always success\n");
+		TEST_TARGET->Run();
+		return true;
+	TEST_UNIT_END;
+}
+
+int main()
+{
+	TestUnit::testMain();
+    return 0;
+}
+
