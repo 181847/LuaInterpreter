77084025990cd3557626e481b81423ff00590782
181847 <753090084@qq.com>
2017-12-01 15:46:54 +0800

    测试LuaInterpreter的限制栈大小的函数以及if函数。

--- LuaInterpreterUnitTest/LuaInterpreterUnitTest.cpp
+++ LuaInterpreterUnitTest/LuaInterpreterUnitTest.cpp
@@ -1,98 +1,46 @@
-// LuaInterpreterUnitTest.cpp : This project is used to test the new LuaFeature。
+// TryUnitTestModule.cpp : 定义控制台应用程序的入口点。
 //
 
 #include "stdafx.h"
-#include "../../Library/Lua/LuaInterpreter/LuaInterpreter.h"
+#include "../../Library/MyTools/UnitTestModules.h"
 #include "../../Library/Lua/LuaModuls/LuaMeshData/LuaMeshDataStruct.h"
+#include "../../Library/Lua/LuaInterpreter/LuaInterpreter.h"
+#include "../../Library/MyTools/MyAssert.h"
 #include "../../Library/MyTools/Formater.h"
 
 #pragma comment(lib, "LuaInterpreter.lib")
 
+DECLARE_TEST_UNITS;
 
-static std::vector<
-	std::function<
-	bool(Lua::LuaInterpreter* pluainter, std::string& unitName)>>
-	testUnits;
+static auto GLuaInterpreter = std::make_unique<Lua::LuaInterpreter>();
 
-// use the next marco to start a test unit,
-// the unit itself is a lambda that will be added to the global vector
-// 'testUntis', the units will be called later.
-// the return value indicate whether the test has been passed.
-#define TEST_UNIT_START(UnitName) testUnits.push_back(std::move(\
-	[](Lua::LuaInterpreter* pluainter, std::string& unitName) -> bool {\
-		unitName = UnitName;
+#define TEST_TARGET GLuaInterpreter
 
-// this macro is only used int the TEST_UNIT_START ~ END
-#define TEST_TARGET pluainter
-
-// end 
-#define TEST_UNIT_END }))
-
-
-
-static std::unique_ptr<Lua::LuaInterpreter> pLuaInter;
-
-static int totalCount = 0;
-static int success = 0;
-
-void execute();
-void runTest();
-void addTestUnit();
-
-int main()
+// this function is declared in the UnitTestModuls.h,
+// we should implements it here.
+void TestUnit::GetReady()
 {
-
-	try
-	{
-		execute();
-	}
-	catch (SimpleException & e)
-	{
-		fprintf(stderr, "ERROR:%s", e.ToString().c_str());
-	}
-
-	getchar();
-
-	return 0;
+	TEST_TARGET->DoFile("Init.lua");
 }
 
-void execute()
+// this function is declared int the UnitTestModuls.h
+// we should implements it here
+void TestUnit::AfterTest()
 {
-	pLuaInter = std::move(std::make_unique<Lua::LuaInterpreter>());
-	pLuaInter->DoFile("Init.lua");
-	addTestUnit();
-	runTest();
-
-	totalCount = testUnits.size();
-	printf("Summarize:\n");
-	printf("TestCount\tsuccess\t\tfailed\n");
-	printf("%d\t\t%d\t\t%d\n", totalCount, success, totalCount - success);
-
-	pLuaInter->Run();
+	TEST_TARGET->Run();
 }
 
-void runTest()
-{
-	std::string unitName;
-	for (auto & testUnit : testUnits)
-	{
-		bool result = testUnit(pLuaInter.get(), unitName);
-		success += result;
-		printf("\t%s\t\t\t%s\n", result ? "success" : "failed", unitName.c_str());
-	}
-}
-
-void addTestUnit()
+void TestUnit::AddTestUnit()
 {
 	TEST_UNIT_START("doFile is called successfully, \
 		if you can see this information")
 		return true;
 	TEST_UNIT_END;
 
-	TEST_UNIT_START("test get unit name")
+	TEST_UNIT_START("test get global name")
 		TEST_TARGET->GetGlobal("test_GetGlobal");
 		int isNum;
-		int itg = lua_tointegerx(pluainter->m_L, -1, &isNum);
+		int itg = lua_tointegerx(TEST_TARGET->m_L, -1, &isNum);
 		return (isNum && itg == 6);
 	TEST_UNIT_END;
 
@@ -126,13 +74,13 @@ void addTestUnit()
 
 	TEST_UNIT_START("test to integer and pop")
 		TEST_TARGET->GetGlobal("test_ToIntegerAndPop_1");
-		int itg = pluainter->ToIntegerAndPop<int>();
+		int itg = TEST_TARGET->ToIntegerAndPop<int>();
 		return itg == 3;
 	TEST_UNIT_END;
 
 	TEST_UNIT_START("test to integer and pop inner type")
 		TEST_TARGET->GetGlobal("test_ToIntegerAndPop_2");
-		int itg = pluainter->ToIntegerAndPop<int>();
+		int itg = TEST_TARGET->ToIntegerAndPop<int>();
 		return itg == 4;
 	TEST_UNIT_END;
 
@@ -191,7 +139,7 @@ void addTestUnit()
 						++error;
 						break;
 					}
-					DEBUG_MESSAGE("key index: %d\n", keyItg);
+					//DEBUG_MESSAGE("key index: %d\n", keyItg);
 				}
 				else
 				{
@@ -211,10 +159,63 @@ void addTestUnit()
 					{
 						++error;
 					}
-					DEBUG_MESSAGE("key field: %s\n", keyStr);
+					//DEBUG_MESSAGE("key field: %s\n", keyStr);
 				}
 	
 			}); // end foreach
+		return error == 0;
+	TEST_UNIT_END;
+	
+	TEST_UNIT_START("traverse table use easy marco")
+		TEST_TARGET->GetGlobal("test_table");
+		int error = 0;
+		TEST_TARGET->Foreach(
+			FOREACH_START
+				// @ pLuaInter
+				int itg = luaInterForeach->ToIntegerAndPop<int>();
+				if (keyIsNumber)
+				{
+					switch (keyItg)
+					{
+					case 1:
+						error += 4 != itg;
+						break;
+
+					case 2:
+						error += 5 != itg;
+						break;
+
+					case 3:
+						error += 6 != itg;
+						break;
+					default:
+						++error;
+						break;
+					}
+					//DEBUG_MESSAGE("key index: %d\n", keyItg);
+				}
+				else
+				{
+					if (0 == strcmp(keyStr, "a"))
+					{
+						error += 1 != itg;
+					}
+					else if (0 == strcmp(keyStr, "b"))
+					{
+						error += 2 != itg;
+					}
+					else if (0 == strcmp(keyStr, "c"))
+					{
+						error += 3 != itg;
+					}
+					else
+					{
+						++error;
+					}
+					//DEBUG_MESSAGE("key field: %s\n", keyStr);
+				}	
+			FOREACH_END
+			); // lua::foreach end
 		return error == 0;
 	TEST_UNIT_END;
 
@@ -254,5 +255,163 @@ void addTestUnit()
 		return error == 0;
 	TEST_UNIT_END;
 
+	TEST_UNIT_START("test push integer")
+		TEST_TARGET->PushInteger(111);
+		return 111 == TEST_TARGET->ToIntegerAndPop();
+		TEST_UNIT_END;
 
+	TEST_UNIT_START("test push number")
+		TEST_TARGET->PushNumber(2.22);
+		return 10e-8 > (2.22 - TEST_TARGET->ToNumberAndPop());
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("test push integer")
+		const char * pts = "unique_test~!@#$%^&*()\n\t";
+		Formater<256> buffer;
+		TEST_TARGET->PushString(pts);
+		TEST_TARGET->ToStringAndClear<buffer.Size>(buffer.bufferPointer());
+		return 0 == strcmp(pts, buffer.bufferPointer());
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("test get the length of the table")
+		UINT length = TEST_TARGET->GetGlobal("test_table")
+									->LengthOfTop<UINT>();
+		return length == 3;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("test get the length of the table inner type")
+		UINT length;
+		TEST_TARGET->GetGlobal("test_table")
+						->LengthOfTop(&length);
+		return length == 3;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("test constrain stack size not to grow up")
+		int preSize = lua_gettop(TEST_TARGET->m_L);
+		int error = 0;
+		
+		int length = 0;
+		TEST_TARGET->ConstainStackSizeMax(
+			CONSTRAIN_START
+				CONSTRAIN_TARGET->GetGlobal("test_table")
+									->LengthOfTop(&length)
+								->Foreach(
+									FOREACH_START
+										// @ pLuaInter
+										int itg = luaInterForeach->ToIntegerAndPop<int>();
+										if (keyIsNumber)
+										{
+											switch (keyItg)
+											{
+											case 1:
+												error += 4 != itg;
+												break;
+
+											case 2:
+												error += 5 != itg;
+												break;
+
+											case 3:
+												error += 6 != itg;
+												break;
+											default:
+												++error;
+												break;
+											}
+											//DEBUG_MESSAGE("key index: %d\n", keyItg);
+										}
+										else
+										{
+											if (0 == strcmp(keyStr, "a"))
+											{
+												error += 1 != itg;
+											}
+											else if (0 == strcmp(keyStr, "b"))
+											{
+												error += 2 != itg;
+											}
+											else if (0 == strcmp(keyStr, "c"))
+											{
+												error += 3 != itg;
+											}
+											else
+											{
+												++error;
+											}
+											//DEBUG_MESSAGE("key field: %s\n", keyStr);
+										}	
+									FOREACH_END
+									); // lua::foreach end
+			CONSTRAIN_END
+		);// end Lua::Constrain...Max()
+		int afterSize = TEST_TARGET->GetStackSize();
+		error += preSize < afterSize;
+		return error == 0;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("test if else condition branch")
+		// for test_ifelse_true
+		int cannotBe123 = 0;
+		int shouldBe456 = 0;
+
+		// for test_ifelse_nil
+		int shouldBe789 = 0;
+		int cannotBe321 = 0;
+		TEST_TARGET
+			->GetGlobal("test_ifelse_true")
+			// this Variable is always true( NOT CONDITION )
+			->If(
+				LUA_INTERPRETER_IF
+					// if correct, here will return false, 
+					// because true is never the nil.
+					return CONDITION_TARGET->IsNil();
+				LUA_INTERPRETER_THEN
+					// if correct, this code will never be called
+					THEN_TARGET
+						->GetGlobal("test_if_true_then")
+						->ToIntegerAndPop(&cannotBe123);
+				LUA_INTERPRETER_ELSE
+					// if correct, this code will be called
+					ELSE_TARGET
+						->GetGlobal("test_if_true_else")
+						->ToIntegerAndPop(&shouldBe456);
+				LUA_INTERPRETER_ENDIF
+			)// end Lua::LuaInterperter::If
+			->GetGlobal("test_ifelse_nil")
+			// this Variable is always nil ( NOT CONDITION )
+			->If(
+				LUA_INTERPRETER_IF
+					// if correct, here will return true, 
+					// because nil is 'nil'.
+					return CONDITION_TARGET->IsNil();
+				LUA_INTERPRETER_THEN
+					THEN_TARGET
+						->GetGlobal("test_if_nil_then")
+						->ToIntegerAndPop(&shouldBe789);
+				LUA_INTERPRETER_ELSE
+					ELSE_TARGET
+						->GetGlobal("test_if_false_then")
+						->ToIntegerAndPop(&cannotBe321);
+				LUA_INTERPRETER_ENDIF
+			);// end Lua::LuaInterepreter::If
+
+		return	cannotBe123 != 123
+			&&	shouldBe456 == 456
+			&&	shouldBe789 == 789
+			&&	cannotBe321 != 321;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("test run the interactive mode")
+		printf("Here have enter the lua interactive mode, please enter the commands to test the interpreter.\n");
+		printf("This unit test will always success\n");
+		TEST_TARGET->Run();
+		return true;
+	TEST_UNIT_END;
 }
+
+int main()
+{
+	TestUnit::testMain();
+    return 0;
+}
+
